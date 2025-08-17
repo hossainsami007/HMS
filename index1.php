527d5440d57b93709aae7948f8bda10eac72e07d
Hafiz Ullah Sayem <hafizullahsayem@gmail.com>
2025-08-18 02:12:29 +0600

    index1.php

---
+++ index1.php
@@ -0,0 +1,35 @@
+?php
+include("header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" type="text/css" href="style2.css">
+
+
+    
+  </head>
+  <style type="text/css">
+    #inputbtn:hover{cursor:pointer;}
+    .card{
+    background: #f8f9fa;
+    border-top-left-radius: 5% 5%;
+    border-bottom-left-radius: 5% 5%;
+    border-top-right-radius: 5% 5%;
+    border-bottom-right-radius: 5% 5%;
+}
+
+  </style>
+  <body style="background: #F0F2F0;background: -webkit-linear-gradient(to bottom, #000C40, #F0F2F0);background: linear-gradient(to bottom, #000C40, #F0F2F0); background-size: cover;">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" >
+    <div class="container">
